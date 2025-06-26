f52dc911c24d16503943039c61cad4cf83f4baf3
mcavol <127151698+mcavol@users.noreply.github.com>
2025-06-27 01:52:19 +0300

    Add files via upload

---
+++ color/color.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+
+def main():
+    yellow = [0, 255, 255]  # yellow in BGR colorspace
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+
+        hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        lowerLimit, upperLimit = get_limits(color=yellow)
+
+        mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+
+        mask_ = Image.fromarray(mask)
+
+        bbox = mask_.getbbox()
+
+        if bbox is not None:
+            x1, y1, x2, y2 = bbox
+
+            frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+
+    cv2.destroyAllWindows()
+
+
+
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
+
+if __name__ == "__main__":
+    main()
